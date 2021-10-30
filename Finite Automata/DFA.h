81f95c2711da66608744bd2d69bae595b3f08954
CamiloOY <amiloc333@gmail.com>
2021-10-30 01:47:21 +0100

    Added product construction to DFA Store states and final states in vector instead of unordered_set

--- Finite Automata/DFA.h
+++ Finite Automata/DFA.h
@@ -9,7 +9,7 @@
 template<typename T>
 class DFA {
 public:
-	DFA(std::unordered_set<T> states, std::unordered_set<char> alphabet, std::function<T(T, char)> transitions, T initial_state, std::unordered_set<T> final_states) : states(states), alphabet(alphabet), transition_function(transitions), initial_state(initial_state), final_states(final_states) { // TODO: Ensure s is in Q and F is a subset of Q
+	DFA(std::vector<T> states, std::unordered_set<char> alphabet, std::function<T(T, char)> transitions, T initial_state, std::vector<T> final_states) : states(states), alphabet(alphabet), transition_function(transitions), initial_state(initial_state), final_states(final_states) { // TODO: Ensure s is in Q and F is a subset of Q
 
 	}
 
@@ -18,13 +18,33 @@ public:
 		for(int c = 0; c < str.length(); c++) {
 			current_state = transition_function(current_state, str[c]);
 		}
-		return final_states.count(current_state);
+		return std::find(final_states.begin(), final_states.end(), current_state) != final_states.end();
+	}
+
+	template<typename U>
+	DFA<std::pair<T, U>> cross(DFA<U> other) {
+		std::vector<std::pair<T, U>> new_states;
+		for(T el_a : states) {
+			for(U el_b : other.states) {
+				new_states.push_back({el_a, el_b});
+			}
+		}
+		std::vector<std::pair<T, U>> new_final_states;
+		for(T el_a : final_states) {
+			for(U el_b : other.final_states) {
+				new_final_states.push_back({el_a, el_b});
+			}
+		}
+		std::function<std::pair<T, U>(std::pair<T, U>, char)> transitions = [=](std::pair<T, U> current_state, char letter) -> std::pair<T, U> {
+			return {this->transition_function(current_state.first, letter), other.transition_function(current_state.second, letter)};
+		};
+		return DFA<std::pair<T, U>>(new_states, alphabet, transitions, {initial_state, other.initial_state}, new_final_states);
 	}
 
 private:
-	std::unordered_set<T> states;
+	std::vector<T> states;
 	std::unordered_set<char> alphabet;
 	std::function<T(T, char)> transition_function;
 	T initial_state;
-	std::unordered_set<T> final_states;
+	std::vector<T> final_states;
 };
--- Finite Automata/main.cpp
+++ Finite Automata/main.cpp
@@ -17,5 +17,20 @@ int main() {
 	for(int i = 0; i < 7; i++) {
 		std::cout << test_strings[i] << ": " << triple_as.acceptsString(test_strings[i]) << '\n';
 	}
+	std::function<int(int, char)> transitions_2 = [](int current_state, char letter) -> int {
+		switch(letter) {
+		case 'a':
+			return (current_state + 1) % 2;
+		case 'b':
+			return current_state;
+		default:
+			throw "Invalid letter";
+		}
+	};
+	DFA<int> double_as({0, 1}, {'a', 'b'}, transitions_2, 0, {0});
+	auto both = triple_as.cross(double_as);
+	for(int j = 0; j < 7; j++) {
+		std::cout << test_strings[j] << ": " << both.acceptsString(test_strings[j]) << '\n';
+	}
 	return 0;
 }
